ea14abd518c59722bcc53782c2bd88bfaac3053b
Ixide <ixide.com@gmail.com>
2025-05-20 09:50:53 -0700

    Initial commit with project structure

---
+++ src/main/java/com/deanery/JavaDocsProject/JavaDocsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.deanery.JavaDocsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDocsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDocsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deanery/JavaDocsProject/JavaDocsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deanery.JavaDocsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDocsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
